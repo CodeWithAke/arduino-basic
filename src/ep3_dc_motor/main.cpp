184b0db32606067596c37781a85038e9ba94bfbe
Ake <e.watcharapong@gmail.com>
2026-04-08 02:45:49 +0700

    refactor main.cpp to improve motor control logic and enhance serial output

--- src/ep3_dc_motor/main.cpp
+++ src/ep3_dc_motor/main.cpp
@@ -1,12 +1,38 @@
 #include <Arduino.h>
 
 
+int MOTOR_1A = 9;
+int MOTOR_1B = 10;
+
 void setup() {
   Serial.begin(9600);
+
+  Serial.println("Hello World");
   
+  pinMode(MOTOR_1A, OUTPUT);
+  pinMode(MOTOR_1B, OUTPUT);
+
+  digitalWrite(MOTOR_1B, LOW);
+
+  Serial.println("Turning motor forward");
 }
 
 void loop() {
-  Serial.println("Hello World");
-  delay(1000);
+  
+  // digitalWrite(MOTOR_1A, HIGH);
+
+  analogWrite(MOTOR_1A, 0);
+  Serial.println("Motor off");
+
+  delay(2000);
+
+  analogWrite(MOTOR_1A, 127);
+  Serial.println("Motor half speed");
+
+  delay(2000);
+
+  analogWrite(MOTOR_1A, 255);
+  Serial.println("Motor full speed");
+
+  delay(2000);
 }
